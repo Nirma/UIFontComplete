fdfac0c4ab8166d26899348edabc1b71a43dfc95
Nicholas Maccharoli <nicko@screaming-cactus.com>
2019-02-24 11:00:45 +0900

    Update copyright date to 2019.

--- UIFontComplete-tvOSTests/UIFontComplete_tvOSTests.swift
+++ UIFontComplete-tvOSTests/UIFontComplete_tvOSTests.swift
@@ -1,7 +1,7 @@
 //
 //  UIFontComplete_tvOSTests.swift
 //
-// Copyright (c) 2016-2018 Nicholas Maccharoli
+// Copyright (c) 2016-2019 Nicholas Maccharoli
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
